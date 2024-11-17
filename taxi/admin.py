b436787c8ad3134c81aaff2c371457a83d524bdb
Volodymyr Yablonskyi <wowa2807@gmail.com>
2024-11-17 11:36:10 +0200

    Solution

---
+++ taxi/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from .models import Car, Manufacturer, Driver
+
+
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'license_number']
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'first_name', 'last_name', 'password')
+        }),
+        ('Additional info', {
+            'classes': ('collapse',),  # Це дозволяє приховувати секцію за замовчуванням
+            'fields': ('license_number',),  # Додаємо поле license_number до цієї секції
+        }),)
+    add_fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'first_name', 'last_name', 'password')
+        }),
+        ('Additional info', {
+            'classes': ('collapse',),
+            'fields': ('license_number',),  # Додаємо поле license_number до цієї секції для форми додавання
+        }),
+    )
+
+
+class CarAdmin(admin.ModelAdmin):
+    search_fields = ['model']
+    list_filter = ['manufacturer']
+
+
+# Register your models here.
+admin.site.register(Manufacturer)
+admin.site.register(Car, CarAdmin)
+admin.site.register(Driver, DriverAdmin)
---
+++ taxi/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=225, unique=True)
+    country = models.CharField(max_length=225)
+
+    def __str__(self):
+        return self.name
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=225, unique=True)
+    first_name = models.CharField(max_length=225)
+    last_name = models.CharField(max_length=225)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=225)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    drivers = models.ManyToManyField(Driver)
+
+    def __str__(self):
+        return self.model
